5a736646be79b12e9f2954e4bf8fd88486d2482d
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-06 16:25:27 +0900

    Title: 겹치지 않게 선분 고르기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240306/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(input())
+lines = []
+
+for _ in range(n):
+    lines.append(list(map(int, input().split())))
+
+lines = sorted(lines, key = lambda x: x[0])
+
+
+res = -int(1e9)
+def dfs(level, arr):
+    global res
+    if level == len(lines):
+        res = max(res, len(arr))
+        return
+    
+    for i in range(len(lines)):
+        if not arr or arr[-1] != lines[i] and arr[-1][1] < lines[i][0]:
+            arr.append(lines[i])
+            dfs(level + 1, arr)
+            arr.pop()
+
+dfs(0, [])
+print(res)
