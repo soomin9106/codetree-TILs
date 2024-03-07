fe2c72e77a72a575a8aae2698bf478d59e957b56
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-07 21:03:40 +0900

    Title: 알파벳과 사칙연산, Time: 5011ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 240307/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,46 @@
+import sys
+
+n = input()
+n_list = list(n)
+
+res = -int(1e9)
+modes = ['+', '-', '*']
+my_modes = []
+my_alphas = []
+
+
+for item in n_list:
+    if item in modes:
+        my_modes.append(item)
+    else:
+        my_alphas.append(item)
+
+changed_nums = [0] * len(my_alphas)
+
+def calc():
+    val = changed_nums[0]
+    idx = 1
+    
+    for mode in my_modes:
+        if mode == '+':
+            val += changed_nums[idx]
+        if mode == '-':
+            val -= changed_nums[idx]
+        if mode == '*':
+            val *= changed_nums[idx]
+        idx += 1
+    # print(changed_nums, val)
+    return val
+
+def dfs(cnt):
+    global res
+    if cnt == len(my_alphas):
+        res = max(res, calc())
+        return 
+    
+    for i in range(1, 5):
+        changed_nums[cnt] = i
+        dfs(cnt + 1)
+
+dfs(0)
+print(res)
