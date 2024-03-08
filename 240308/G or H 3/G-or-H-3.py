6e2da2fab0512d85d561fd00cf67b3d8bab18e9e
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-08 10:29:03 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240308/G or H 3/G-or-H-3.py
+++ 240308/G or H 3/G-or-H-3.py
@@ -3,16 +3,16 @@ import sys
 n, k = map(int, input().split())
 max_position = 0  # 최대 위치 변수 추가
 arr = [''] * 10001
-points_list = []
+points_set = set()
 
 for _ in range(n):
     point, alpha = map(str, input().split())
     point = int(point)
-    points_list.append(point)
+    points_set.add(point)
     arr[point] = alpha
     max_position = max(max_position, point)
 
-points_list = sorted(points_list)
+points_list = sorted(list(points_set))  # set을 list로 변환하여 정렬
 
 res = -int(1e9)
 
