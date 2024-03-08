1bfbae41312c9c102b7d05d4983328042f46ae8c
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-08 18:57:43 +0900

    Title: 바구니 안의 사탕 2, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240308/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240308/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,12 +1,12 @@
 import sys 
 
 n, k = map(int, input().split())
-buckets = [(i, 0) for i in range(101)]
+buckets = [0 for i in range(101)]
 max_point = -int(1e9)
 for _ in range(n):
     cnt, point = map(int, input().split())
     max_point = max(max_point, point)
-    buckets[point] = (point, cnt)
+    buckets[point] += cnt
 
 # print(buckets)
 
@@ -17,7 +17,7 @@ for i in range(max_point + 1):
     for j in range(i - k, i + k + 1):
         if j < 1 or j > 100:
             break
-        val += buckets[j][1]
+        val += buckets[j]
 
     res = max(res, val)
 
