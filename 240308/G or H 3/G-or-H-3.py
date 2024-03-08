0250c71a9f5eaaa5fd637150a1d7314799b49ef4
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-08 10:19:23 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240308/G or H 3/G-or-H-3.py
+++ 240308/G or H 3/G-or-H-3.py
@@ -9,27 +9,21 @@ for _ in range(n):
     points_list.append(int(point))
     arr[int(point)] = alpha
 
-points_list.sort()
+points_list = sorted(points_list)
 
 res = -int(1e9)
 
-if len(points_list) == 1 and k == 1:
-    if arr[points_list[0]] == 'G':
-        print(1)
-        exit(0)
-    else:
-        print(2)
-        exit(0)
+
 
 for point in points_list:
-    if point + k in points_list:
-        val = 0
-        for i in range(point, point + k + 1):
-            if arr[i] == 'G':
-                val += 1
-            elif arr[i] == 'H':
-                val += 2
-            else:
-                continue
+    val = 0
+    for i in range(point, point + k + 1):
+        if arr[i] == 'G':
+            val += 1
+        elif arr[i] == 'H':
+            val += 2
+        else:
+            continue
         res = max(res, val)
+        
 print(res)
