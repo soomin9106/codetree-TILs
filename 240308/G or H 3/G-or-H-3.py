893646fe6cd66a41c001a01a1d4a0cd2e0534d95
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-08 10:35:39 +0900

    Title: G or H 3, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240308/G or H 3/G-or-H-3.py
+++ 240308/G or H 3/G-or-H-3.py
@@ -3,21 +3,23 @@ import sys
 n, k = map(int, input().split())
 max_position = 0  # 최대 위치 변수 추가
 arr = [''] * 10001
-points_set = set()
+points_list = []
 
 for _ in range(n):
     point, alpha = map(str, input().split())
     point = int(point)
-    points_set.add(point)
+    points_list.append(point)
     arr[point] = alpha
     max_position = max(max_position, point)
 
-points_list = sorted(list(points_set))  # set을 list로 변환하여 정렬
+points_list = sorted(points_list)
 
 res = -int(1e9)
 
 for idx in range(len(points_list)):
     val = 0
+    if points_list[idx] + k >= 10000:
+        break
     for i in range(points_list[idx], points_list[idx] + k + 1):
         if arr[i] == 'G':
             val += 1
