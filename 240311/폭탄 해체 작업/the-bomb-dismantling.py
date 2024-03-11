0552e04a03e7955485ba0bbf3113a66fcd258a20
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-11 18:55:36 +0900

    Title: 폭탄 해체 작업, Time: 246ms, Memory: 28MB, Status: Passed - CodeTree

--- 240311/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240311/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,3 +1,4 @@
+import heapq
 n = int(input())
 
 arr = []
@@ -5,16 +6,29 @@ for _ in range(n):
     score, limit = map(int, input().split())
     arr.append((limit, score))
 
-arr = sorted(arr, key = lambda x: (x[0], -x[1]))
+arr = sorted(arr, key = lambda x: x[0])
 
-cur_val = arr[0]
-res = cur_val[1]
 
-for i in range(1, len(arr)):
-    if arr[i][0] == cur_val[0]:
-        continue
-    else:
-        res += arr[i][1]
-        cur_val = arr[i]
 
-print(res)
+def get_time_limit(idx):
+    limit, _ = arr[idx]
+    return limit
+
+def get_scroe(idx):
+    _, score = arr[idx]
+    return score
+
+MAX_T = 10000
+bomb_idx = n - 1
+pq = []
+ans = 0
+
+for i in range(MAX_T, 0, -1):
+    while bomb_idx >= 0 and get_time_limit(bomb_idx) >= i:
+        heapq.heappush(pq, -get_scroe(bomb_idx))
+        bomb_idx -= 1
+    
+    if pq:
+        ans += -heapq.heappop(pq)
+
+print(ans)
