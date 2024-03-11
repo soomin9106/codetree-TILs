d665aee04adf8446471b6042647b78f18eb11f15
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-11 23:15:59 +0900

    Title: 자연수 M/2개의 쌍, Time: 479ms, Memory: 30MB, Status: Passed - CodeTree

--- 240311/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 240311/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -3,16 +3,34 @@ m = 0
 arr = []
 for _ in range(n):
     x, y = map(int, input().split())
-    arr += [y] * x 
+    arr.append((x, y))
     m += x
 
 
-arr.sort()
-# print(arr, m)
+arr = sorted(arr, key = lambda x: x[1])
+
+# print(arr)
 
 res = -int(1e9)
-for i in range(m // 2):
-    val = arr[i] + arr[m - i -1]
-    res = max(res, val)
+cur_idx = 0
+comp_idx = n - 1
+
+while cur_idx <= comp_idx:
+    
+    cur_cnt, cur_val = arr[cur_idx]
+    comp_cnt, comp_val = arr[comp_idx]
+
+    if cur_cnt == comp_cnt:
+        res = max(res, cur_val + comp_val)
+        comp_idx -= 1
+        cur_idx += 1
+    elif cur_cnt < comp_cnt:
+        res = max(res, cur_val + comp_val)
+        arr[comp_idx] = (comp_cnt - cur_cnt, comp_val)
+        cur_idx += 1
+    else:
+        res = max(res, cur_val + comp_val)
+        arr[cur_idx] = (cur_cnt - comp_cnt, cur_val)
+        comp_idx -= 1
 
 print(res)
