8528ce3cc5b4b1117d2e3fbeb43c53a6797e3c74
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-11 17:35:49 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 329ms, Memory: 31MB, Status: Passed - CodeTree

--- 240311/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240311/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -1,30 +1,25 @@
 from collections import deque
 
 n = int(input())
-b_arr = []
-a_arr = []
+b_cards = []
 for _ in range(n):
-    b_arr.append(int(input()))
+    b_cards.append(int(input()))
+b_set = set(b_cards)
+a_cards = [
+    num
+    for num in range(1, 2 * n + 1)
+    if num not in b_set
+]
 
+a_cards.sort()
+b_cards.sort()
 
-for i in range(1, 2 * n + 1):
-    if i in b_arr:
-        continue
-    else:
-        a_arr.append(i)
-
-a_arr.sort()
-
-res = 0
+ans = 0
+b_idx = 0
 
 for i in range(n):
-    if a_arr and min(a_arr) > b_arr[i]:
-        a_arr.pop(0)
-        res += 1
-    if a_arr and min(a_arr) < b_arr[i] and max(a_arr) < b_arr[i]:
-        a_arr.pop(0)
-    if a_arr and max(a_arr) > b_arr[i]:
-        a_arr.pop()
-        res +=1
+    if b_idx < n and a_cards[i] > b_cards[b_idx]:
+        ans += 1
+        b_idx += 1
 
-print(res)
+print(ans)
