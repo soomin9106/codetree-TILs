e47764b2e26225133ca5eae5333b8f8ba7b14ab4
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-14 14:09:22 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240314/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,35 @@
+n, t = map(int, input().split())
+
+command = input()
+
+arrs = []
+for _ in range(3):
+    arrs.append(list(map(int, input().split())))
+
+# 오른쪽 돌기 기준
+dxs = [-1, 0, 1, 0]
+dys = [0, 1, 0, -1]
+
+
+x, y = n//2, n//2
+d = 0
+res = arrs[x][y]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+
+for i in range(t):
+    if command[i] == 'F':
+        nx, ny = x + dxs[d], y + dys[d]
+        if in_range(nx, ny):
+            x, y = nx, ny
+            res += arrs[x][y]
+
+    elif command[i] == 'R':
+        d = (d + 1) % 4
+
+    else:
+        d = (d - 1) if d != 0 else 3
+
+print(res)
