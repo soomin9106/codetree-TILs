8f224620ef39df89f493d86e0d9f0d3becb50c42
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-14 16:57:51 +0900

    Title: 마을 구분하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240314/마을 구분하기/seperate-village.py
@@ -0,0 +1,46 @@
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+
+
+visited = [[False] * n for _ in range(n)]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+dxs = [0, 0, 1, -1]
+dys = [1, -1, 0, 1]
+
+def dfs(x, y):
+    global cnt
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+
+        if in_range(nx, ny) and not visited[nx][ny] and arr[nx][ny] == 1:
+            visited[nx][ny] = True
+
+            cnt += 1
+            dfs(nx, ny)
+
+
+cnt = 0
+people = []
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1 and not visited[i][j]:
+            visited[i][j] = True
+            cnt = 1
+
+            dfs(i, j)
+
+            people.append(cnt)
+
+
+people.sort()
+print(len(people))
+
+for p in people:
+    print(p)
