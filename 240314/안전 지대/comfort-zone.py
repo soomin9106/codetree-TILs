2ef3a0eed160fb257728f7046d68f2026dcb5158
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-14 17:35:48 +0900

    Title: 안전 지대, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240314/안전 지대/comfort-zone.py
@@ -0,0 +1,51 @@
+n, m = map(int, input().split())
+
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+temp = arr
+
+k = 1
+max_safe_zone = 0
+
+visited = [[False] * m for _ in range(n)]
+
+dxs = [1, -1, 0, 0]
+dys = [0, 0, 1, -1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+def dfs(x, y):
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+
+        if in_range(nx, ny) and not visited[nx][ny] and temp[nx][ny] != 0:
+            visited[nx][ny] = True
+            dfs(nx, ny)
+
+
+while True:
+    temp = arr
+    cnt = 0
+    visited = [[False] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == k:
+                temp[i][j] = 0
+
+    for i in range(n):
+        for j in range(m):
+            if not visited[i][j] and temp[i][j] != 0:
+                visited[i][j] = True
+                cnt += 1
+                dfs(i, j)
+
+    if cnt <= max_safe_zone:
+        break
+    else:
+        max_safe_zone = cnt
+        k += 1
+
+print(k - 1, max_safe_zone)
