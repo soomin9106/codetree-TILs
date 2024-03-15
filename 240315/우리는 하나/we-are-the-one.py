97b540fcd4e5270a1a8d11e4213e75f198468dac
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-15 22:38:24 +0900

    Title: 우리는 하나, Time: 290ms, Memory: 27MB, Status: Passed - CodeTree

--- 240315/우리는 하나/we-are-the-one.py
+++ 240315/우리는 하나/we-are-the-one.py
@@ -8,7 +8,6 @@ for _ in range(n):
 
 max_res = -int(1e9)
 q = deque()
-visited = [[False] * n for _ in range(n)]
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
@@ -17,16 +16,20 @@ dxs = [1, -1, 0, 0]
 dys = [0, 0, 1, -1]
 
 # BFS 함수 수정
-def bfs():
+def bfs(combination):
+    visited = [[False] * n for _ in range(n)]
+    for x, y in combination:
+        visited[x][y] = True
+        q.append((x, y))
+
     while q:
         cur_x, cur_y = q.popleft()
         for dx, dy in zip(dxs, dys):
             nx, ny = cur_x + dx, cur_y + dy
 
             if in_range(nx, ny) and not visited[nx][ny] and u <= abs(arr[nx][ny] - arr[cur_x][cur_y]) <= d:
-                # cnt += 1
                 q.append((nx, ny))
-                visited[nx][ny] = True  # 방문한 도시 체크
+                visited[nx][ny] = True
 
     cnt = 0
     for i in range(n):
@@ -36,30 +39,19 @@ def bfs():
     return cnt
 
 # 백트래킹을 통해 k 개의 점을 고르기
-selected = []
-check = [[False] * n for _ in range(n)]
 def select_k(l, r, c, combination):
     global max_res
     if l == k:
-        # selected.append(combination[:])  
-        visited = [[False] * n for _ in range(n)]
-        for x, y in combination:
-            visited[x][y] = True  # 방문한 도시 체크
-            q.append((x, y))
-        count = bfs()
+        count = bfs(combination)
         max_res = max(max_res, count)
         return 
 
     for i in range(r, n):
         start_col = c if i == r else 0 
         for j in range(start_col, n):
-            if not check[i][j]:
-                check[i][j] = True
-                combination.append((i, j))
-                select_k(l + 1, i, j, combination) 
-                check[i][j] = False
-                combination.pop()
-
+            combination.append((i, j))
+            select_k(l + 1, i, j, combination) 
+            combination.pop()
 
 select_k(0, 0, 0, [])
 
