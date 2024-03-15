7cef55cb8289dedd1a189798df2b70573e9542bc
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-15 10:04:01 +0900

    Title: 갈 수 있는 곳들, Time: 182ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240315/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+n, k = map(int, input().split())
+arr = []
+
+visited = [[False] * n for _ in range(n)]
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+points = []
+for _ in range(k):
+    r, c = map(int, input().split())
+    points.append((r - 1, c - 1))
+
+dxs = [1, -1, 0, 0]
+dys = [0, 0, 1, -1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+
+    if visited[x][y]:
+        return False
+
+    if arr[x][y] == 1:
+        return False
+
+    return True
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        cur_x, cur_y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = cur_x + dx, cur_y + dy
+
+            if can_go(nx, ny):
+                visited[nx][ny] = True
+                q.append((nx, ny))
+
+
+for point in points:
+    x, y = point
+    bfs(x, y)
+
+cnt = 0
+
+for i in range(n):
+    for j in range(n):
+        if visited[i][j]:
+            cnt += 1
+
+print(cnt)
