238318532cdd5f9efeca31e094c82137c171c825
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-15 09:49:47 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 316ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240315/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+visited = [[False] * m for _ in range(n)]
+
+dxs = [1, -1, 0, 0]
+dys = [0, 0, 1, -1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    
+    if visited[x][y]:
+        return False
+    
+    if arr[x][y] == 0:
+        return False
+
+    return True
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        cur_x, cur_y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = cur_x + dx, cur_y + dy
+
+            if can_go(nx, ny):
+                visited[nx][ny] = True
+                q.append((nx, ny))
+
+
+bfs(0, 0)
+print(1 if visited[n-1][m-1] else 0)
