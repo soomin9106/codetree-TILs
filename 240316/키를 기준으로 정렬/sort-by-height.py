81cecdce75df3e4535090d625169fd6b0aad5fa1
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-16 13:56:52 +0900

    Title: 키를 기준으로 정렬, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240316/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+n = int(input())
+students = []
+for _ in range(n):
+    name, height, weight = map(str, input().split())
+    students.append(Student(name, int(height), int(weight)))
+
+students = sorted(students, key = lambda x: x.height)
+
+for s in students:
+    print(s.name, s.height, s.weight)
