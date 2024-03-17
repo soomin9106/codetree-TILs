5789f98289636a550cf9d076fab54ee0992fa91f
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-17 14:49:30 +0900

    Title: 행복한 수열의 개수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240317/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,38 @@
+n, m = map(int, input().split())
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+
+def is_happy_sequence(ar):
+    cnt = 1
+    for i in range(1, len(ar)):
+        if cnt >= m:
+            return True
+        if ar[i] == ar[i - 1]:
+            cnt += 1
+
+            if cnt >= m:
+                return True
+        else:
+            cnt = 1
+
+    return False
+
+res = 0
+
+for i in range(n):
+    # print(arr[i])
+    if is_happy_sequence(arr[i]):
+        res += 1
+
+for i in range(n):
+    new_arr = []
+    for j in range(n):
+        new_arr.append(arr[j][i])
+    # print(new_arr)
+    if is_happy_sequence(new_arr):
+        res += 1
+
+print(res)
