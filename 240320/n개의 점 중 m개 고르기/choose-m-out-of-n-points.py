3a723739c7b3e2327206c2277fcb49652ff2025a
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-20 21:41:07 +0900

    Title: n개의 점 중 m개 고르기, Time: 512ms, Memory: 28MB, Status: Passed - CodeTree

--- 240320/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240320/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -10,17 +10,32 @@ for _ in range(n):
 
 selected_points = []
 res = int(1e9)
+distance = -int(1e9)
+selecteds = []
 
-def calc():
-    x1, y1 = selected_points[0]
-    x2, y2 = selected_points[1]
-    distance = (x2 - x1)**2 + (y2 - y1)**2
-    return int(distance)
+def calc(cur_idx, cnt):
+    global distance, selecteds
+    if cnt == 2:
+        # print(selecteds)
+        x1, y1 = selecteds[0]
+        x2, y2 = selecteds[1]
+        distance = max(distance, (x1-x2)**2 + (y1-y2)**2)
+        return
+
+    if cur_idx == len(selected_points):
+        return
+
+    calc(cur_idx + 1, cnt)
+    selecteds.append(selected_points[cur_idx])
+    calc(cur_idx+1, cnt+1)
+    selecteds.pop()
 
 def dfs(cur_idx, cnt):
-    global res, selected_points
-    if cnt == 2:
-        res = min(res, calc())
+    global res, selected_points, distance
+    if cnt == m:
+        distance = -int(1e9)
+        calc(0, 0)
+        res = min(res, distance)
         return 
     
     if cur_idx == n:
