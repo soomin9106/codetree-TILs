c745b2df20aa718fbb9d8e862c4602790707158a
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-20 17:54:19 +0900

    Title: xor 결과 최대 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240320/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+selected_nums = []
+res = -int(1e9)
+
+def calc_xor():
+    # selected_nums 에 있는 값들 xor 계산
+    val = selected_nums[0]
+
+    for i in range(1, len(selected_nums)):
+        val ^= selected_nums[i]
+    
+    return val
+
+def dfs(idx):
+    global selected_nums 
+    global res
+    if len(selected_nums) == m - 1:
+        res = max(calc_xor(), res)
+        return 
+    
+    if idx >= n:
+        return
+
+    selected_nums.append(arr[idx])
+    dfs(idx + 1)
+    selected_nums.pop()
+
+    dfs(idx + 1)
+
+dfs(0)
+print(res)
