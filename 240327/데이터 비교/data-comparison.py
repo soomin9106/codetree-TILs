6853e2188e7bf2e86f3d1fa33a6807b50afe647f
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-27 21:57:14 +0900

    Title: 데이터 비교, Time: 206ms, Memory: 41MB, Status: Passed - CodeTree

---
+++ 240327/데이터 비교/data-comparison.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr1 = list(map(int, input().split()))
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+setArr1 = set(arr1)
+
+for a in arr2:
+    if a in setArr1:
+        print(1, end = ' ')
+    else:
+        print(0, end = ' ')
