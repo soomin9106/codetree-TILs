327057626a61d583bb9a3e3b2a413c721ff2fb1f
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-28 16:27:20 +0900

    Title: 초대장과 번호표, Time: 310ms, Memory: 40MB, Status: Passed - CodeTree

--- 240328/초대장과 번호표/invitation-and-number-tag.py
+++ 240328/초대장과 번호표/invitation-and-number-tag.py
@@ -1,23 +1,40 @@
+from collections import deque
+
 n, g = map(int, input().split())
 
-invited = set()
-invited.add(1)
+invited = [False] * n
+# 각 그룹마다 초대장 받지 못한 사람
+groups = [set() for _ in range(g)]
+# 각 사람이 어떤 그룹에 속하는지
+people_groups = [[] for _ in range(n)]
 
-for _ in range(g):
-    group = list(map(int, input().split()))
-    k = group[0]
+q = deque()
+ans = 0
 
-    count = 0
-    not_invited = []
+for i in range(g):
+    nums = list(map(int, input().split()))[1:]
 
-    for i in range(1, len(group)):
-        if group[i] in invited:
-            count += 1
-        else:
-            not_invited.append(group[i])
+    for num in nums:
+        num -= 1
+        groups[i].add(num)
+        people_groups[num].append(i)
 
-    if count == k - 1:
-        for ni in not_invited:
-            invited.add(ni)
 
-print(len(invited))
+q.append(0)
+invited[0] = True
+
+while q:
+    cur = q.popleft()
+    ans += 1
+
+    for g_num in people_groups[cur]: # cur 이 소속된 그룹 번호들
+        groups[g_num].remove(cur) # cur 을 삭제 해줌
+
+        if len(groups[g_num]) == 1: # 해당 그룹 사람이 1명이 되었을 경우 => 초대
+            p_num = list(groups[g_num])[0]
+
+            if not invited[p_num]:
+                invited[p_num] = True
+                q.append(p_num)
+
+print(ans)
