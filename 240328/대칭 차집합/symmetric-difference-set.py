34e2186bb9093c885d58158fd04d85ff33de8935
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-28 16:41:35 +0900

    Title: 대칭 차집합, Time: 437ms, Memory: 70MB, Status: Passed - CodeTree

--- 240328/대칭 차집합/symmetric-difference-set.py
+++ 240328/대칭 차집합/symmetric-difference-set.py
@@ -3,19 +3,4 @@ alen, blen = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-# 식으로는 이거임
-# set(a-b | b-a)
-
-# a - b 
-ab = []
-for item in a:
-    if item not in b:
-        ab.append(item)
-
-# b - a
-ba = []
-for item in b:
-    if item not in a:
-        ba.append(item)
-
-print(len(set(ab + ba)))
+print(len(set(a) - set(b)) + len(set(b) - set(a)))
