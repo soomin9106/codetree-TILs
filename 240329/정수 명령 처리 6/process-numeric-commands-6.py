ef3906e300a6c93d8deb8a5ce43d42edb61b399f
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-03-29 14:58:01 +0900

    Title: 정수 명령 처리 6, Time: 396ms, Memory: 29MB, Status: Passed - CodeTree

--- 240329/정수 명령 처리 6/process-numeric-commands-6.py
+++ 240329/정수 명령 처리 6/process-numeric-commands-6.py
@@ -1,28 +1,30 @@
 import heapq
 
 class PriorityQueue:
-    def __init__(self):
+    def __init__(self):          # 빈 Priority Queue 하나를 생성합니다.
         self.items = []
-
-    def push(self, item):
+                
+    def push(self, item):        # 우선순위 큐에 데이터를 추가합니다.
         heapq.heappush(self.items, -item)
-
-    def size(self):
-        return len(self.items)
-
-    def empty(self):
+                
+    def empty(self):             # 우선순위 큐가 비어있으면 True를 반환합니다.
         return not self.items
-
-    def pop(self):
+                
+    def size(self):              # 우선순위 큐에 있는 데이터 수를 반환합니다.
+        return len(self.items)
+        
+    def pop(self):               # 우선순위 큐에 있는 데이터 중 최댓값에 해당하는 데이터를 반환하고 제거합니다.
         if self.empty():
-            raise Exception("empty queue")
+            raise Exception("PriorityQueue is empty")
+            
         return -heapq.heappop(self.items)
-
-    def top(self):
+                
+    def top(self):               # 우선순위 큐에 있는 데이터 중 최댓값에 해당하는 데이터를 제거하지 않고 반환합니다.
         if self.empty():
             raise Exception("PriorityQueue is empty")
                         
         return -self.items[0]
+
 
 pq = PriorityQueue()
 n = int(input())
@@ -38,5 +40,5 @@ for _ in range(n):
         print(pq.size())
     if commands[0] == 'empty':
         print(1 if pq.empty() else 0)
-    if commands == 'top':
+    if commands[0] == 'top':
         print(pq.top())
