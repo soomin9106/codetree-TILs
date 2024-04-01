baaa0ec0443657a4f66da84518ddb957e4bb4130
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-01 13:55:47 +0900

    Title: 가장 가까운 점, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240401/가장 가까운 점/nearest-point.py
@@ -0,0 +1,23 @@
+import heapq
+
+n, m = map(int, input().split())
+
+points = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+pq = []
+
+def calc_dist(x, y):
+    return abs(x) + abs(y)
+
+for point in points:
+    x, y = point
+    heapq.heappush(pq, (calc_dist(x, y), x, y))
+
+    _, nx, ny = heapq.heappop(pq)
+
+    heapq.heappush(pq, (calc_dist(nx + 2, ny + 2), nx + 2, ny + 2))
+
+print(pq[0][1], pq[0][2])
