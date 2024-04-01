fe4973519563866a1e72bc4464ef8cfa7ae4e436
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-01 17:14:57 +0900

    Title: k번째로 작은 쌍의 합, Time: 281ms, Memory: 33MB, Status: Wrong Answer - CodeTree

--- 240401/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 240401/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -13,18 +13,23 @@ pq = []
 for item in arrn:
     heapq.heappush(pq, (item + arrm[0], item, arrm[0]))
 
-cnt = 0
+cnt = 1
 n_idx = 0
 m_idx = 1
 res = 0
 
-while cnt < k:
+
+while cnt != k:
+    # print(n_idx, m_idx)
     res = heapq.heappop(pq)
     cnt += 1
 
     heapq.heappush(pq, (arrn[n_idx] + arrm[m_idx], arrn[n_idx], arrm[m_idx]))
+
     n_idx = (n_idx + 1) % len(arrn)
     if n_idx == 0:
         m_idx += 1
 
+
+res = heapq.heappop(pq)
 print(res[0])
