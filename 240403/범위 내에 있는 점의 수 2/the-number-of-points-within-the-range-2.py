19f1c222cc35cc79b0e20c7a65594f5791730a8b
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-03 18:42:53 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 427ms, Memory: 45MB, Status: Passed - CodeTree

--- 240403/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 240403/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -4,23 +4,21 @@ points = list(map(int, input().split()))
 
 max_point = max(points)
 
+nums = [0] * (max_point + 1)
+for point in points:
+    nums[point] = 1
+
 prefix_sum = [0] * (max_point + 1)
 for i in range(1, max_point + 1):
-    flag = 0
-    if i in points:
-        flag = 1
-    
-    prefix_sum[i] = prefix_sum[i - 1] + flag
+    prefix_sum[i] = prefix_sum[i - 1] + nums[i]
 
 answer = 0
 for _ in range(q):
     s, e= map(int, input().split())
-    flags = 0
-    if s in points:
-        flags = 1
+
     if s > max_point:
         print(0)
     elif e > max_point and s < max_point:
-        print(max(prefix_sum) - prefix_sum[s] + flags)
+        print(max(prefix_sum) - prefix_sum[s] + nums[s])
     else:
-        print(prefix_sum[e] - prefix_sum[s] + flags)
+        print(prefix_sum[e] - prefix_sum[s] + nums[s])
