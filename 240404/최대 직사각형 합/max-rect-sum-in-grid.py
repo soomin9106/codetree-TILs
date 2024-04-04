9e3331e5c89bda731f7e99468aa8084885f69424
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-04 11:21:24 +0900

    Title: 최대 직사각형 합, Time: 402ms, Memory: 27MB, Status: Passed - CodeTree

--- 240404/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 240404/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -22,14 +22,25 @@ def get_sum(s1, e1, s2, e2):
                 prefix_sum[s1 - 1][e2] - \
                 prefix_sum[s2][e1 - 1] + \
                 prefix_sum[s1 - 1][e1 - 1]
+
+# 시작행 r1, 끝 행 r2
+def get_max_rect(r1, r2):
+    dp = [0] * (n + 1)
+
+    for y in range(1, n + 1):
+        val = get_sum(r1, y, r2, y)
+        dp[y] = max(val, dp[y - 1] + val)
+
+    max_area = -int(1e9)
+    for y in range(1, n + 1):
+        max_area = max(max_area, dp[y])
+
+    return max_area
     
 
 answer = -int(1e9)
-for w in range(1, n + 1):
-    for h in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                if in_range(i + w -1, j + h - 1):
-                    answer = max(answer, get_sum(i, j, i + w -1, j + h - 1))
+for i in range(1, n + 1):
+    for j in range(i, n + 1):
+        answer = max(answer, get_max_rect(i, j))
 
 print(answer)
