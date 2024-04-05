d25bc9ad4000e492d288ace1ed3b3d2877e099d9
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-05 10:24:33 +0900

    Title: 떨어지는 1자 블록, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240405/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,19 @@
+n, m, k = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+k -= 1
+cur_row = 0
+
+for i in range(n):
+    if sum(arr[i][k: k+m]) == 0:
+        cur_row = i
+
+for j in range(k, k + m):
+    arr[cur_row][j] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end = ' ')
+    print()
