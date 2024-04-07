60486639e19bfe5a89ac53249a4e91dd6849162f
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-07 21:05:47 +0900

    Title: 불안한 무빙워크, Time: 414ms, Memory: 28MB, Status: Passed - CodeTree

--- 240407/불안한 무빙워크/unstable-moving-walk.py
+++ 240407/불안한 무빙워크/unstable-moving-walk.py
@@ -1,9 +1,7 @@
 n, k = map(int, input().split())
 
-# 안정성 - 사람 유무 체크
 u = [(0, False) for _ in range(n)]
 d = [(0, False) for _ in range(n)]
-
 
 def shift():
     temp = u[n - 1]
@@ -15,37 +13,52 @@ def shift():
 
     for i in range(n - 1, 0, -1):
         d[i] = d[i - 1]
-
+    
     d[0] = temp
 
 def can_go(idx):
     if idx == n:
-        return True
+        return True 
 
     s, o = u[idx]
-    # 안정성이 0 이상이고, 사람이 그곳에 없어야 이동 가능
     return s > 0 and not o
 
 def move(idx):
-    cur_s, _ = u[idx]
-    u[idx] = (cur_s, False)
+    sta, occupied = u[idx]
+
+    u[idx] = (sta, False)
 
     if idx + 1 < n:
-        n_s, _ = u[idx + 1]
-        u[idx + 1] = (n_s - 1, True)
-
+        ns, _ = u[idx + 1]
+        u[idx + 1] = (ns - 1, True)
 
 def move_all():
-    for i in range(n -1 , -1, -1):
-        _, o = u[i]
-        if o and can_go(i + 1):
+    for i in range(n - 1, -1, -1):
+        cur_s, cur_o = u[i]
+
+        if cur_o and can_go(i + 1):
             move(i)
 
 def add():
     s, o = u[0]
 
-    if s > 0 and not o:
+    if not o and s > 0:
         u[0] = (s - 1, True)
+
+def done():
+    k_cnt = 0
+    for item in u:
+        s, o = item
+        if s == 0:
+            k_cnt += 1
+
+    for item in d:
+        s, o = item
+
+        if s == 0:
+            k_cnt += 1
+
+    return k_cnt >= k
 
 def simulate():
     shift()
@@ -54,31 +67,16 @@ def simulate():
 
     add()
 
-    _, o = u[n -1]
+    _, o = u[n - 1]
     if o:
         move(n - 1)
 
-def done():
-    unstable_cnt = 0
-
-    for s, _ in u:
-        if not s:
-            unstable_cnt += 1
-
-    for s, _ in d:
-        if not s:
-            unstable_cnt += 1
-
-    return unstable_cnt >= k 
-
-row = list(map(int, input().split()))
-for i, s in enumerate(row[:n]):
-    u[i] = (s, False)
-
-for i, s in enumerate(row[n:]):
-    d[i] = (s, False)
-
 answer = 0
+given_row = list(map(int, input().split()))
+for i, stability in enumerate(given_row[:n]):
+    u[i] = (stability, False)
+for i, stability in enumerate(given_row[n:]):
+    d[i] = (stability, False)
 
 while not done():
     simulate()
