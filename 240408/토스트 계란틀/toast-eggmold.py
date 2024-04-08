aa41600d135dcacdea673d4d76bf1cda42480e7f
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-08 11:14:15 +0900

    Title: 토스트 계란틀, Time: 765ms, Memory: 28MB, Status: Passed - CodeTree

--- 240408/토스트 계란틀/toast-eggmold.py
+++ 240408/토스트 계란틀/toast-eggmold.py
@@ -9,11 +9,13 @@ for _ in range(n):
 dxs = [1, -1, 0, 0]
 dys = [0, 0, 1, -1]
 
+visited = [[False] * n for _ in range(n)]
+
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
 
-def bfs(x, y, visited):
+def bfs(x, y):
     points = []
     q = deque()
 
@@ -32,26 +34,31 @@ def bfs(x, y, visited):
                 points.append((nx, ny))
                 visited[nx][ny] = True
 
-    sum_val = 0
+    sum_val = sum([
+        arr[x][y]
+        for x, y in points
+    ])
 
     for p in points:
-        sum_val += arr[p[0]][p[1]]
-
-    avg = sum_val // len(points)
-
-    for p in points:
-        arr[p[0]][p[1]] = avg
+        arr[p[0]][p[1]] = sum_val // len(points)
 
     return len(points)
 
+# visited 배열을 초기화 해줍니다.
+def initialize_visited():
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
 
 def simulate():
-    visited = [[False] * n for _ in range(n)]
+    initialize_visited()
+
     isChanged = False
+
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                count = bfs(i, j, visited)
+                count = bfs(i, j)
                 if count > 1:
                     isChanged = True
 
@@ -62,7 +69,7 @@ def simulate():
     return True
 
 answer = 0
-for _ in range(2):
+for _ in range(2000):
     isChanged = simulate()
 
     # for i in range(n):
