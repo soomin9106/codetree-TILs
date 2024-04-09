67555dd8bfa93ae7558b136846d889d5d380aa26
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-09 23:26:45 +0900

    Title: 방화벽 설치하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240409/방화벽 설치하기/firewall-installation.py
+++ 240409/방화벽 설치하기/firewall-installation.py
@@ -6,30 +6,22 @@ arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-new_arr = [[0] * m for _ in range(n)]
-
 dxs = [1, -1, 0, 0]
 dys = [0, 0, 1, -1]
 
-# 불 2, 방화벽 1, 빈칸 0
-visited = [[False] * m for _ in range(n)]
-visited_bfs = [[False] * m for _ in range(n)]  # BFS 탐색을 위한 visited 배열
-
 def initialize_new_arr(chosen_walls):
-    for i in range(n):
-        for j in range(m):
-            new_arr[i][j] = arr[i][j]
-
+    new_arr = [row[:] for row in arr]  # 기존 배열 복사
     for (x, y) in chosen_walls:
         new_arr[x][y] = 1
+    return new_arr
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
 
-def calc(chosen_walls):
-    initialize_new_arr(chosen_walls)
-    
+def bfs_fire(new_arr):
+    visited_bfs = [[False] * m for _ in range(n)]
     bfs_q = deque()
+
     for i in range(n):
         for j in range(m):
             if new_arr[i][j] == 2:
@@ -46,30 +38,29 @@ def calc(chosen_walls):
                 visited_bfs[nx][ny] = True
                 bfs_q.append((nx, ny))
 
-    val = 0
-
+    ans = 0
     for i in range(n):
-        for j in range(m):
-            if not visited_bfs[i][j] and new_arr[i][j] == 0:
-                val += 1
+        for j in range(n):
+            if new_arr[i][j] == 0 and not visited_bfs[i][j]:
+                ans += 1
+    
+    return ans
 
-    return val
 
 ans = 0
 def choose_walls(x_idx, y_idx, chosen_walls):
     global ans
     if len(chosen_walls) == 3:
-        val = calc(chosen_walls)
+        new_arr = initialize_new_arr(chosen_walls)
+        val = bfs_fire(new_arr)
         ans = max(ans, val)
         return 
 
     for i in range(x_idx, n):
-        y_idx = y_idx if i == x_idx else 0
-        for j in range(y_idx, m):
-            if not visited[i][j] and arr[i][j] == 0:
-                visited[i][j] = True
+        y_start = y_idx if i == x_idx else 0
+        for j in range(y_start, m):
+            if arr[i][j] == 0:
                 choose_walls(i, j + 1, chosen_walls + [(i, j)])
-                visited[i][j] = False
 
 choose_walls(0, 0, [])
 print(ans)
