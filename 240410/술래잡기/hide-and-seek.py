81c26b0352ce7104e7a48997943f201e1bf634ee
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-10 19:18:50 +0900

    Title: 술래잡기, Time: 295ms, Memory: 30MB, Status: Passed - CodeTree

--- 240410/술래잡기/hide-and-seek.py
+++ 240410/술래잡기/hide-and-seek.py
@@ -1,177 +1,180 @@
 n, m, h, k = map(int, input().split())
 
+hiders = [
+    [[] for _ in range(n)]
+    for _ in range(n)
+]
+
+next_hiders = [
+    [[] for _ in range(n)]
+    for _ in range(n)
+]
+
+tree = [
+    [False] * n
+    for _ in range(n)
+]
+
+seeker_next_dir = [
+    [0] * n
+    for _ in range(n)
+]
+
+seeker_rev_dir = [
+    [0] * n
+    for _ in range(n)
+]
+
+seeker_pos = (n // 2, n // 2)
+forward_facing = True
+
 ans = 0
 
-runners = []
 for _ in range(m):
     x, y, d = map(int, input().split())
-    cur_d = 1 if d == 1 else 3
-    runners.append((x - 1, y - 1, d, cur_d))
+    hiders[x-1][y-1].append(d)
 
-trees = []
 for _ in range(h):
     x, y = map(int, input().split())
-    trees.append((x - 1 , y - 1))
+    tree[x-1][y-1] = True
 
-# 술래 좌표 (x, y)
-catcher = (n // 2, n // 2)
-is_forward = True
-catcher_next_dir = [
-    [0] * n
-    for _ in range(n)
-]
-
-catcher_reverse_next_dir = [
-    [0] * n
-    for _ in range(n)
-]
-
-# 좌우상하 순서
-dxs = [0, 0, -1, 1]
-dys = [-1, 1, 0, 0]
-
-def get_distance(x1, y1, x2, y2):
-    return abs(x1 - x2) + abs(y1 - y2)
-
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
-
-# 도망자의 도망치기
-def run(x, y, d, cur_d):
-    cur_x, cur_y = x, y
-    nd = cur_d
-    nx, ny = cur_x + dxs[cur_d], cur_y + dys[cur_d]
-    cx, cy = catcher[0], catcher[1]
-
-    # 다음칸이 격자가 벗어나지 않는 경우
-    if in_range(nx, ny):
-        # 움직이려는 칸에 술래가 있는 경우
-        if (nx, ny) != (cx, cy):
-            cur_x, cur_y = nx, ny
-    else:
-        if cur_d == 1:
-            nd = 0
-        elif cur_d == 0:
-            nd = 1
-        elif cur_d == 3:
-            nd = 2
-        else:
-            nd = 3
-
-        nx, ny = cur_x + dxs[nd], cur_y + dys[nd]
-
-        if (nx, ny) != (cx, cy):
-            cur_x, cur_y = nx, ny
-
-    
-    return cur_x, cur_y, nd # 이동한 위치와 방향 반환
-
-# 모든 도망자 조건에 맞게 움직이기
-def run_all():
-    global runners
-
-    for i in range(len(runners)):
-        x, y, d, cur_d = runners[i]
-        if get_distance(x, y, catcher[0], catcher[1]) <= 3:
-            nx, ny, nd = run(x, y, d, cur_d)
-            runners[i] = (nx, ny, d, nd)
-
-    
-
-
-# 술래의 경로 계산 (달팽이 모양)
-def initialize_catcher_path():
+def initialize_seeker_path():
     dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
 
     cur_x, cur_y = n // 2, n // 2
     move_dir, move_num = 0, 1
 
-    for _ in range(move_num):
-        catcher_next_dir[cur_x][cur_y] = move_dir
-        cur_x, cur_y = cur_x + dxs[move_dir], cur_y + dys[move_dir]
-        catcher_reverse_next_dir[cur_x][cur_y] = move_dir + 2 if move_dir < 2 else move_dir - 2
+    while cur_x or cur_y:
+        for _ in range(move_num):
+            seeker_next_dir[cur_x][cur_y] = move_dir
+            cur_x, cur_y = cur_x + dxs[move_dir], cur_y + dys[move_dir]
+            seeker_rev_dir[cur_x][cur_y] = move_dir + 2 if move_dir < 2 else move_dir - 2
 
-        if not cur_x and not cur_y:
-            break
+            if not cur_x and not cur_y:
+                break
 
         move_dir = (move_dir + 1) % 4
         if move_dir == 0 or move_dir == 2:
             move_num += 1
 
-def get_catcher_dir():
-    x, y = catcher
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def hider_move(x, y, move_dir):
+    dxs, dys = [0, 0, 1, -1], [-1, 1, 0, 0]
+
+    nx, ny = x + dxs[move_dir], y + dys[move_dir]
+
+    if not in_range(nx, ny):
+        move_dir = 1 - move_dir if move_dir < 2 else 5 - move_dir
+        nx, ny = x + dxs[move_dir], y + dys[move_dir]
+
+    if (nx, ny) != seeker_pos:
+        next_hiders[nx][ny].append(move_dir)
+    else:
+        next_hiders[x][y].append(move_dir)
+
+def dist_from_seeker(x, y):
+    seeker_x, seeker_y = seeker_pos
+    return abs(seeker_x - x) + abs(seeker_y - y)
+
+def hider_move_all():
+    for i in range(n):
+        for j in range(n):
+            next_hiders[i][j] = []
+
+    for i in range(n):
+        for j in range(n):
+            if dist_from_seeker(i, j) <= 3:
+                for move_dir in hiders[i][j]:
+                    hider_move(i, j, move_dir)
+
+            else:
+                for move_dir in hiders[i][j]:
+                    next_hiders[i][j].append(move_dir)
+
+    
+    for i in range(n):
+        for j in range(n):
+            hiders[i][j] = next_hiders[i][j]
+
+def get_seeker_dir():
+    x, y = seeker_pos
 
     move_dir = 0
 
-    if is_forward:
-        move_dir = catcher_next_dir[x][y]
+    if forward_facing:
+        move_dir = seeker_next_dir[x][y]
     else:
-        move_dir = catcher_reverse_next_dir[x][y]
+        move_dir = seeker_rev_dir[x][y]
 
     return move_dir
 
 def check_facing():
-    global is_forward
+    global forward_facing
 
-    if catcher == (0, 0) and is_forward:
-        is_forward = False
+    if seeker_pos == (0, 0) and forward_facing:
+        forward_facing = False
 
-    if catcher == (n // 2, n // 2) and not is_forward:
-        is_forward = True
+    if seeker_pos == (n // 2, n // 2) and not forward_facing:
+        forward_facing = True
 
-def catcher_move():
-    global catcher
 
-    x, y = catcher
+def seeker_move():
+    global seeker_pos
 
-    # 상우하좌 순서대로 넣어줍니다.
+    x, y = seeker_pos
+
     dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
 
-    move_dir = get_catcher_dir()
+    move_dir = get_seeker_dir()
 
-    catcher = (x + dxs[move_dir], y + dys[move_dir])
+    seeker_pos = (x + dxs[move_dir], y + dys[move_dir])
 
     check_facing()
 
-def find_catchers(x, y):
-    global runners
-    val = 0
-    idxs = []
-    for idx, (curx, cury, d, cur_d) in enumerate(runners):
-        if (x, y) == (curx, cury):
-            val += 1
-            idxs.append(idx)
-
-    for i in idxs:
-        runners.pop(i)
-
-    return val
-
 def get_score(t):
-    global ans
+    global ans 
 
     # 상우하좌 순서대로 넣어줍니다.
     dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
 
-    x, y = catcher
+    # 현재 술래의 위치를 불러옵니다.
+    x, y = seeker_pos
+    
+    # 술래의 방향을 불러옵니다.
+    move_dir = get_seeker_dir()
 
-    move_dir = get_catcher_dir()
-
+    # 3칸을 바라봅니다.
     for dist in range(3):
         nx, ny = x + dist * dxs[move_dir], y + dist * dys[move_dir]
-        if in_range(nx, ny) and (nx, ny) not in trees:
-            val = find_catchers(nx, ny)
-            ans += t * val
+        
+        # 격자를 벗어나지 않으며 나무가 없는 위치라면 
+        # 도망자들을 전부 잡게 됩니다.
+        if in_range(nx, ny) and not tree[nx][ny]:
+            # 해당 위치의 도망자 수 만큼 점수를 얻게 됩니다.
+            ans += t * len(hiders[nx][ny])
+
+            # 도망자들이 사라지게 됩니다.
+            hiders[nx][ny] = []
 
 def simulate(t):
-    run_all()
+    # 도망자가 움직입니다.
+    hider_move_all()
 
-    catcher_move()
-
+    # 술래가 움직입니다.
+    seeker_move()
+    
+    # 점수를 얻습니다.
     get_score(t)
 
-initialize_catcher_path()
 
+# 술래잡기 시작 전에
+# 구현상의 편의를 위해
+# 술래 경로 정보를 미리 계산합니다.
+initialize_seeker_path()
+
+# k번에 걸쳐 술래잡기를 진행합니다.
 for t in range(1, k + 1):
     simulate(t)
 
