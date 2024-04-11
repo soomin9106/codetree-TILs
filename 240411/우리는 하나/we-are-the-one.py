43116dc694cea3f2c91517487737a028013503f5
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-11 22:39:09 +0900

    Title: 우리는 하나, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240411/우리는 하나/we-are-the-one.py
@@ -0,0 +1,83 @@
+from collections import deque
+
+n, k, u, d = map(int, input().split())
+
+maps = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+points = [
+    (x, y)
+    for x in range(n)
+    for y in range(n)
+]
+
+dxs = [-1, 1, 0, 0]
+dys = [0, 0, -1, 1]
+
+visited = [[False] * n for _ in range(n)]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def can_go(x, y, val):
+    return in_range(x, y) and \
+            not visited[x][y] and \
+            u <= abs(maps[x][y] - val) <= d
+
+def initialize_visited():
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
+
+
+def bfs():
+    initialize_visited()
+    bfs_q = deque()
+    for (x, y) in selected_points:
+        visited[x][y] = True 
+        bfs_q.append((x, y))
+
+    while bfs_q:
+        curx, cury = bfs_q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = curx + dx, cury + dy
+
+            if can_go(nx, ny, maps[curx][cury]):
+                visited[nx][ny] = True
+                bfs_q.append((nx, ny))
+
+def calc():
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]:
+                cnt += 1
+
+    return cnt
+
+selected_points = []
+ans = -1
+def choose_points(idx):
+    global ans
+    if len(selected_points) == k:
+        bfs()
+        count = calc()
+        
+        ans = max(ans, count)
+        return 
+
+    if idx >= n:
+        return 
+
+    choose_points(idx + 1)
+
+    selected_points.append(points[idx])
+    choose_points(idx + 1)
+    selected_points.pop()
+
+
+choose_points(0)
+print(ans)
