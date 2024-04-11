b6b038ab174f15477cab05d7c5619fed272cbe8f
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-11 11:37:38 +0900

    Title: 그래프 탐색, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240411/그래프 탐색/graph-traversal.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+visited = [False] * (n + 1)
+
+def dfs(vertex):
+    for cur_v in graph[vertex]:
+        if not visited[cur_v]:
+            visited[cur_v] = True
+            dfs(cur_v)
+
+dfs(1)
+
+cnt = 0
+
+for i in range(1, n + 1):
+    if visited[i]:
+        cnt += 1
+
+cnt -= 1
+print(cnt)
