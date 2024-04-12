1d9bb048b0fbca44311ba75dbd7b1b07c599016a
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-12 22:31:26 +0900

    Title: 괄호 쌍 만들어주기, Time: 170ms, Memory: 25MB, Status: Passed - CodeTree

--- 240412/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240412/괄호 쌍 만들어주기/pair-parentheses.py
@@ -1,34 +1,18 @@
 a = input()
 
-open_sets = []
-close_sets = []
 n = len(a)
 
-for i in range(len(a)):
-    if a[i] == "(":
-        open_sets.append(i)
+R = [0] * n
 
-opens = []
+for i in range(n -2, -1, -1):
+    if a[i] == ")" and a[i + 1] == ")":
+        R[i] = R[i + 1] + 1
+    else:
+        R[i] = R[i + 1]
 
-for i in range(len(open_sets) - 1):
-    if abs(open_sets[i] - open_sets[i + 1]) == 1:
-        opens.append((open_sets[i], open_sets[i + 1]))
-
-for i in range(len(a)):
-    if a[i] == ")":
-        close_sets.append(i)
-
-closes = []
-
-for i in range(len(close_sets) - 1):
-    if abs(close_sets[i] - close_sets[i + 1]) == 1:
-        closes.append((close_sets[i], close_sets[i + 1]))
-
-# print(opens, closes)
 ans = 0
-for (x , y) in opens:
-    for (cx, cy) in closes:
-        if y < cx:
-            ans += 1
+for i in range(0, n - 2, 1):
+    if a[i] == "(" and a[i + 1] == "(":
+        ans += R[i + 2]
 
 print(ans)
