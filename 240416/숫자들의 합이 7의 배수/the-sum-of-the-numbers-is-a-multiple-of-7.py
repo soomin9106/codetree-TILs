6d0912b5396357422c6c49673b2ee4409eeefe90
SUMIN <79344555+soomin9106@users.noreply.github.com>
2024-04-16 12:08:57 +0900

    Title: 숫자들의 합이 7의 배수, Time: 234ms, Memory: 28MB, Status: Passed - CodeTree

--- 240416/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
+++ 240416/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
@@ -7,15 +7,26 @@ for _ in range(n):
 
 prefix_sum = [0] * (n + 1)
 
+# 구간 [i, j] 에서
+# prefix_sum[j] - prefix_sum[i - 1] 이 7의 배수가 되어야 함
+r = dict()
+
 for i in range(1, n + 1):
     prefix_sum[i] = ((prefix_sum[i - 1] % 7) + arr[i]) % 7
 
-# print(prefix_sum)
+for i in range(1, n + 1):
+    if prefix_sum[i] not in r:
+        r[prefix_sum[i]] = (i, int(1e9))
+
+    else:
+        minimum, _ = r[prefix_sum[i]]
+        r[prefix_sum[i]] = (minimum, i)
+
 
 ans = 0
-for i in range(1, n + 1):
-    for j in range(i + 1, n + 1):
-        if prefix_sum[j] == prefix_sum[i - 1]:
-            ans = max(j - i + 1, ans)
+for key in r:
+    mini, maxi = r[key]
+    if maxi != int(1e9):
+        ans = max(ans, maxi - mini)
 
 print(ans)
